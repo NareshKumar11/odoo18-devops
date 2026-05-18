619ca5e4c279317aa78183476b508aa5526f8e2b
Naresh P <Naresh.P@mindsparktechnologies.com>
2026-05-18 10:36:27 +0530

    Add hospital patient model

--- custom_addons/hospital/models/models.py
+++ custom_addons/hospital/models/models.py
@@ -1,19 +1,15 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class hospital(models.Model):
-#     _name = 'hospital.hospital'
-#     _description = 'hospital.hospital'
+class hospital(models.Model):
+    _name = 'hospital.hospital'
+    _description = 'hospital.hospital'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    name = fields.Char()
+    value = fields.Integer()
+    value2 = fields.Float()
+    description = fields.Text()
+
 
